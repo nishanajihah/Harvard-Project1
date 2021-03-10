305a9846d752ce16aea4e9700dfe5454bf9af0ea
Nisha Najihah <nishanajihah88@gmail.com>
2021-03-10 18:54:15 +0800

    Creating the title entry page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
+
+mark = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def page_direct(request, title):
+
+    page_entry = util.get_entry(title)
+    
+    return render(request, "encyclopedia/entries_page.html", {
+        'page': mark.cover(page_entry),
+        'title': title,
+    })
