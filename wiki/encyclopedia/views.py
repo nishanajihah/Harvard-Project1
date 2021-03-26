01be784acec343a998e6c06f44b6ef91521d2109
Nisha Najihah <nishanajihah88@gmail.com>
2021-03-26 18:02:20 +0800

    updating the search code

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -9,23 +9,26 @@ from . import util
 from markdown2 import Markdown
 markdowner = Markdown()
 
+
 class searchForm (forms.Form):
     title = forms.CharField(label="", widget=forms.TextInput(attrs={
       "class": "search",
       "placeholder": "Search Encyclopedia"}))
+
 
 def index(request):
 
     # Home
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        # "search_form" : searchForm()    
+        # "search_form" : searchForm()
     })
+
 
 def page(request, title):
 
     md_file = util.get_entry(title)
-    
+
     if md_file != None:
 
         # If page exist convert md file to html and display
@@ -43,20 +46,39 @@ def page(request, title):
             "message": f"Error: '{title}' page was not found."
         })
 
+
 def search(request):
 
     entries = util.list_entries()
+    search = []
     query = request.GET.get("q", "")
 
-    # Query is exists
     if query in entries:
-        return redirect(page, query)  
+        md_file = util.get_entry(query)
+        convert_md = markdowner.convert(md_file)
+
+        page_content = {
+            'title': query,
+            'page': convert_md,
+        }
+
+        return render(request, "enc:page", page_content)  
+    
+    if query != entries:
+        return redirect(page, query)
 
     results = [page for page in entries if query.lower() in page.lower()]
-
     return render(request, "encyclopedia/search_page.html", {
         "entries": results,
     })
+  
+
+    
+
+    
+
+
+   
 
 
     
